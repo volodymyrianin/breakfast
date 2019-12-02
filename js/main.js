d10e455c1142a600cf2669300fc21d795c4aed63
volodymyrianin <volodymyrianin1@gmail.com>
2019-12-02 20:26:52 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-;(function(){
+;(function($){
+	$(function(){
+		var $slider = $('.yva-section-4__slider');
+		$slider.slick({
+			dots: true,
+			dotsClass: 'yva-section-4__slider-dots',
+			arrows: false
+		});
+	})
 
-})();
+})(jQuery);
